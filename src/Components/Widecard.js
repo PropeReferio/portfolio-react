dfbfb7fdcbb981b3c586f53c230f29a34258a42c
PropeReferio <lemuel.b.stevens@gmail.com>
2020-04-21 17:19:31 -0500

    opens in new tab

--- src/Components/Widecard.js
+++ src/Components/Widecard.js
@@ -8,7 +8,8 @@ class Widecard extends React.Component{
                     <img src={this.props.img} className='projectimg' alt='Project'></img>
                 </imgwrapper>
                 <div class="compdet">
-                    <h3><a href={this.props.url}>{this.props.title}</a></h3>
+                    <h3><a href={this.props.url} target="_blank" rel="noopener noreferrer">
+                        {this.props.title}</a></h3>
                     <h4 class="secondtext">{this.props.where}</h4>
                     <h4 class="secondtext">{this.props.from} {this.props.to}</h4>
                     <p>{this.props.desc}</p>
